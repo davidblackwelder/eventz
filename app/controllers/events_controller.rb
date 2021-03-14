ecedeeacc7085dd67260c5a3cea7eea7fd971f86
David Blackwelder <github@davidblackwelder.com>
2021-03-13 19:26:59 -0500

    Add capacity and image file name to database and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -37,6 +37,6 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event)
-          .permit(:name, :description, :location, :price, :starts_at)
+          .permit(:name, :description, :location, :price, :starts_at, :capacity, :image_file_name)
   end
 end
