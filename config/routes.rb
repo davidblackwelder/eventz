edff99480a216e3e7ce912685ec5dfa4ed957f9c
David Blackwelder <github@davidblackwelder.com>
2021-03-19 04:48:25 -0400

    Display a sign-in form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-
   root 'events#index'
   resources :events do
     resources :registrations
   end
+
+  resource :session, only: [:new, :create, :destroy]
 
   resources :users
   get 'signup' => 'users#new'
