e52b49b9630fd929f038b4c28a9386819f99d250
David Blackwelder <github@davidblackwelder.com>
2021-03-20 13:46:30 -0400

    Allows only the current user to edit their own user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -25,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -38,13 +37,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to events_url, alert: "Account successfully deleted!"
   end
 
   private
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to events_url unless current_user?(@user)
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
